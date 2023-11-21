2529001e86db628d13507522e1be7275ae9c2a68
Júlia Silva de Araújo - 823122431 <honeyedits06@gmail.com>
2023-11-21 10:53:15 -0300

    Classes pedido e produto criadas Classes pedido e produto criada

---
+++ Ecommerce/src/main/java/com/mycompany/ecommerce/Pedido.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ecommerce;
+
+/**
+ *
+ * @author 823122431
+ */
+public class Pedido {
+    private static void main(String[] args) {
+        JOption.pane.showMessageDialog(null, "Bem vindo ao seu carrinho, edite e adicione aqui o que desejar!");
+       JOption.pane.showInputDialog(null, "Produtos adicionados até agora, digite ok para prosseguir");
+       
+       int Celular {
+               id.produto(); 
+               preco.produto(); 
+               quantidade.produto(); 
+    }
+        int Geladeira {
+                id.produto();
+                preco.produto();
+                quantidade.produto();
+    }
+               
+                int Livro{
+                id.produto();
+                preco.produto();
+                quantidade.produto();
+    }
+               
+               int Tablet {
+                id.produto();
+                preco.produto();
+                quantidade.produto();
+    }
+               Método de Pagamento
+                       int cartao.debito();
+                       message("Cadastre seu cartão de debito");
+                       int numero.debito();
+                       String nome.debito();
+                       int seguranca.debito();
+                       int validade.debito()
+                               
+                       int cartao.credito();
+                       message("Cadastre seu cartão de crédito"); 
+                       int numero.credito();
+                       String nome.credito();
+                       int seguranca.credito();
+                       int validade.credito();
+                       
+    }
+    private String carrinho de compras; 
+    
+    
+}
